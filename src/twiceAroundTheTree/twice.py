e51c7f007ad30246e056eaca2f34c73e70b782e0
ddogs <ddogs@LinuxUbuntu.myguest.virtualbox.org>
2023-12-10 14:14:57 -0300

    Created functions to process the tsp instances and write the results to output file within a time limit

--- src/twiceAroundTheTree/twice.py
+++ src/twiceAroundTheTree/twice.py
@@ -2,6 +2,10 @@ import networkx as nx
 import numpy as np
 import os
 import re
+import time
+import multiprocessing
+import signal
+import psutil
 
 def TSPTwiceAroundTheTree(graphAdjacencyMatrix):
     
@@ -68,3 +72,73 @@ def tspInstanceToNumOfNodesMapper(tspInstances):
             mapResult[int(match)] = tspInstance
 
     return mapResult
+
+def runTspWithTimeout(graphAdjacencyMatrix, timeout, resultQueue):
+    def handler(signum, frame):
+        raise TimeoutError("Function execution timed out")
+
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(timeout)
+    
+    try:
+        best, sol = TSPTwiceAroundTheTree(graphAdjacencyMatrix)
+        resultQueue.put((best, sol, None))
+    except Exception as e:
+        resultQueue.put((None, None, e))
+    finally:
+        signal.alarm(0)  # Cancel the alarm
+
+def processTspInstance(tspInstance, outputFile, tspFolderPath):
+    
+    tspFilePath = os.path.join(tspFolderPath, tspInstance, tspInstance + ".tsp")
+    startReadDataTime = time.time()
+    graphAdjacencyMatrix = readTspData(tspFilePath)
+    endReadDataTime = time.time()
+    durationReadDataTime = endReadDataTime - startReadDataTime
+
+    outputFile.write(f"Time taken to read the file: {durationReadDataTime} seconds\n")
+    outputFile.write("-----------\n")
+
+    startMemoryUsageInKB = psutil.Process(os.getpid()).memory_info().rss / 1024
+    startAlgorithmExecuteTime = time.time()
+
+    # Set the timeout to 30 minutes (1800 seconds)
+    algorithmExecutionTimeLimitSeconds = 1800
+
+    resultQueue = multiprocessing.Queue()
+    process = multiprocessing.Process(target=runTspWithTimeout, args=(graphAdjacencyMatrix, algorithmExecutionTimeLimitSeconds, resultQueue))
+    process.start()
+    process.join(algorithmExecutionTimeLimitSeconds)  # Wait for the process to finish or timeout
+
+    if process.is_alive():
+        # If the process is still running, terminate it
+        process.terminate()
+        process.join()
+        outputFile.write("Time limit reached. No solution found.\n")
+        endAlgorithmExecuteTime = time.time()
+        endMemoryUsageInKB = psutil.Process(os.getpid()).memory_info().rss / 1024
+        durationAlgorithmExecutionTime = endAlgorithmExecuteTime - startAlgorithmExecuteTime
+        memoryUsage = endMemoryUsageInKB - startMemoryUsageInKB
+
+        outputFile.write(f"Time taken to execute: {durationAlgorithmExecutionTime} seconds\n")
+        outputFile.write(f"Memory taken to execute: {memoryUsage} kilobytes\n")
+        return "Time limit reached. No solution found."
+
+    bestCostFound, bestSolutionFound, error = resultQueue.get()
+
+    endAlgorithmExecuteTime = time.time()
+    endMemoryUsageInKB = psutil.Process(os.getpid()).memory_info().rss / 1024
+    durationAlgorithmExecutionTime = endAlgorithmExecuteTime - startAlgorithmExecuteTime
+    memoryUsage = endMemoryUsageInKB - startMemoryUsageInKB
+
+    outputFile.write(f"Time taken to execute: {durationAlgorithmExecutionTime} seconds\n")
+    outputFile.write(f"Memory taken to execute: {memoryUsage} kilobytes\n")
+
+    if error:
+        outputFile.write(f"An error occurred: {error}\n")
+        return "!!!!!!!!!!!!!!An error occurred!!!!!!!!!!!!!!"
+
+    outputFile.write(f"Best cost: {bestCostFound}\n")
+    outputFile.write(f"Solution path: {bestSolutionFound}\n")
+
+    return "Successfully processed"
